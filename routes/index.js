854c15559cdef76a406a062e39a4b083aadac7b9
Mack <1390mack@gmail.com>
2024-10-12 22:44:12 +0800

    equments and add button

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/equipments', function(req, res, next) {
+  res.render('equipments', { title: 'Equipments' });
+});
+
+router.get('/equipments/add', function(req, res, next) { 
+  res.render('add', { title: 'Add Equipment' });
+});
+
+
 module.exports = router;
