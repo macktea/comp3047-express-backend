9bae33c674e0bb747743d817f991263ec176efad
Mack <1390mack@gmail.com>
2025-03-27 22:34:03 +0800

    security of key storing

--- utils/db.js
+++ utils/db.js
@@ -1,15 +1,16 @@
+require('dotenv').config();
+const { MongoClient, ObjectId } = require('mongodb');
 const { MongoClient, ObjectId } = require('mongodb');
 
-process.env.MONGODB_URI = 'mongodb://comp3047mak:kwXK5sclx4PcQ92Us4RyTvXWzl93FCOu72U1OfJnNOpXkRdj2V95aFUI0oNXN9T6OCfugdhR3aRlACDbqZrsjg==@comp3047mak.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@comp3047mak@';
-
 if (!process.env.MONGODB_URI) {
-    // throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-    process.env.MONGODB_URI = 'mongodb://localhost:27017';
+    throw new Error('Please define the MONGODB_URI environment variable.');
 }
 
-// Connect to MongoDB
 async function connectToDB() {
-    const client = await MongoClient.connect(process.env.MONGODB_URI);
+    const client = await MongoClient.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
     const db = client.db('rent_equipment');
     db.client = client;
     return db;
